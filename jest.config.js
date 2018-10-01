b21583e7b9cf41583e0a05f7d8629d3f3141b9ae
dielduarte <magdiel.kesller@gmail.com>
2018-10-02 01:00:14 +0200

    config(jest): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/config/enzyme.js'],
-  setupFiles: ['<rootDir>/config/enzyme.js']
+  coveragePathIgnorePatterns: ['/node_modules/', '/config/setupTests.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFiles: ['<rootDir>/config/setupTests.js'],
+  collectCoverageFrom: [
+    '<rootDir>/components/**/**.js',
+    '!<rootDir>/components/**/{index,mdx}.js'
+  ]
 };
