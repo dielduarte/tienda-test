2bac6f3a81c126ab7e5edce487d434b193a108e4
dielduarte <magdiel.kesller@gmail.com>
2018-10-04 01:53:37 +0200

    fix(coments): remove comments

--- components/Forms/Editor.spec.js
+++ components/Forms/Editor.spec.js
@@ -4,11 +4,6 @@ import 'jest-styled-components';
 import Editor, { onEditorStateUpdate } from './Editor';
 
 describe('Editor', () => {
-  // it('should render correctly', () => {
-  //   const wrapper = mount(<Editor label="Description" />);
-  //   expect(wrapper).toMatchSnapshot();
-  // });
-
   it('should call setEditorState prop passing the new state', () => {
     const props = { setEditorState: jest.fn(), onChange: jest.fn() };
     const editorState = { newData: '<p>Hey</p>' };
